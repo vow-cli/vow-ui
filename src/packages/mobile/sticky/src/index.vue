edbe7849f577afb9e726e1e6353b0886a2ae6e53
1340100932@qq.com <1340100932@qq.com>
2021-07-28 23:35:33 +0800

    ✨ feat(Sticky): 完成sticky组件部分功能

--- src/packages/mobile/sticky/src/index.vue
+++ src/packages/mobile/sticky/src/index.vue
@@ -4,9 +4,29 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
+
+type modeType = PropType<'js' | 'css'>
+
 export default defineComponent({
   name: 'VowSticky',
+  props: {
+    mode: {
+      type: String as modeType,
+      validator: (val: string) => {
+        return ['css', 'js'].includes(val)
+      },
+      default: 'js',
+    },
+    useWindow: {
+      type: Boolean,
+      default: true,
+    },
+    containerId: {
+      type: String,
+      default: '',
+    },
+  },
   setup() {
     return {}
   },
--- src/packages/mobile/stickyitem/src/index.vue
+++ src/packages/mobile/stickyitem/src/index.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="vow-sticky-item">吸顶容器子组件</div>
+  <div class="vow-sticky-item">
+    <!-- 用于占位,因为header吸顶后脱离文档流，导致组件高度会减小 -->
+    <div class="vow-sticky-item__header-warpper"></div>
+    <div class="vow-sticky-item__header">
+      <slot name="header"></slot>
+    </div>
+    <div class="vow-sticky-item__body">
+      <slot name="body"></slot>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'VowStickyItem',
+  props: {
+    top: {
+      type: Number,
+      default: 0,
+    },
+  },
   setup() {
     return {}
   },
